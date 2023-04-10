caf96aadd459e84d8b50fe181b3a344700031b9b
Sanish Thapa <61194194+Sanish73@users.noreply.github.com>
2023-04-11 00:32:38 +0545

    slider

--- src/components/SlideShowBox.js
+++ src/components/SlideShowBox.js
@@ -1,63 +1,62 @@
-import {
-    Box,
-    HStack,
-  } from 'native-base';
-  import React, { useState, useRef, useEffect } from 'react';
-  import Slideshow from 'react-native-slideshow';
-  
-  function SliderShowBox() {
-    const [position, setPosition] = useState(0);
-    const slideshowRef = useRef(null);
-  
-    const handlePositionChanged = (position) => {
-      setPosition(position);
-      slideshowRef.current.setPosition(position);
-    };
-  
-    useEffect(() => {
-      const interval = setInterval(() => {
-        const newPosition = position === 2 ? 0 : position + 1;
-        setPosition(newPosition);
-        slideshowRef.current.setPosition(newPosition);
-      }, 20000);
-  
-      return () => clearInterval(interval);
-    }, [position]);
-  
-    return (
-      <HStack paddingBottom={5} paddingTop={2}>
-        <Box borderRadius={8} overflow="hidden">
-          <Slideshow
-            ref={slideshowRef}
-            dataSource={[
-              {
-                title: 'Title 1',
-                caption: 'Caption 1',
-                url: 'http://placeimg.com/320/240/any',
-              },
-              {
-                title: 'Title 2',
-                caption: 'Caption 2',
-                url: 'http://placeimg.com/320/240/any',
-              },
-              {
-                title: 'Title 3',
-                caption: 'Caption 3',
-                url: 'http://placeimg.com/320/240/any',
-              },
-            ]}
-            height={100}
-            width={200}
-            position={position}
-            onPositionChanged={handlePositionChanged}
-            indicatorSize={8}
-            containerStyle={{ borderRadius: 8 }}
-            arrowSize={0}
-          />
-        </Box>
-      </HStack>
-    );
-  }
-  
-  export default SliderShowBox;
-  
+import { View } from 'native-base';
+import React, {Component} from 'react';
+import Slideshow from 'react-native-slideshow';
+
+export default class SlideshowTest extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            position: 1,
+            interval: null,
+            dataSource: [
+                {
+                    title: 'Title 1',
+                    caption: 'Caption 1',
+                    url: 'http://placeimg.com/640/480/any'
+                }, {
+                    title: 'Title 2',
+                    caption: 'Caption 2',
+                    url: 'http://placeimg.com/640/480/any'
+                }, {
+                    title: 'Title 3',
+                    caption: 'Caption 3',
+                    url: 'http://placeimg.com/640/480/any'
+                }
+            ]
+        };
+    }
+
+    componentWillMount() {
+        this.setState({interval: setInterval(() => {
+                this.setState({
+                    position: this.state.position === this.state.dataSource.length
+                        ? 0
+                        : this.state.position + 1
+                });
+            }, 7000)});
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.state.interval);
+    }
+
+    render() {
+        return (
+            <View
+            bgColor={'red.100'}
+            m={2}
+                style={{
+                borderRadius: 8,
+                overflow: 'hidden'
+            }}>
+                <Slideshow
+                    dataSource={this.state.dataSource}
+                    position={this.state.position}
+                    onPositionChanged={position => this.setState({position})}
+                    height={123}
+                    width={200}/>
+            </View>
+        );
+    }
+}
