e7478f6d56bc9641ff2ab5d5c2990a764979a786
Sanish Thapa <61194194+Sanish73@users.noreply.github.com>
2023-04-23 16:41:29 +0545

    er

--- src/components/CategorySubCatDetailScree.js
+++ src/components/CategorySubCatDetailScree.js
@@ -1,24 +1,11 @@
 import React, {useState, useEffect, useRef} from 'react';
-import {
-    View,
-    Text,
-    StatusBar,
-    ScrollView,
-    TouchableOpacity,
-    Image
-} from 'react-native';
-import {COLOURS, Items} from '../database/Database';
-import Entypo from 'react-native-vector-icons/Entypo';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import {Box, VStack, Button} from 'native-base';
-import {ProductCard} from './ProductCard';
 import {Animated} from 'react-native';
 import CategoryLeftAnimation from './CategoryLeftAnimation';
 import CategoryRightAnimation from './CategoryRightAnimation';
 
-export function CategorySubCatDetailScree({route}) {
 
+export function CategorySubCatDetailScree({route}) {
     const [currentTab,
         setCurrentTab] = useState("Home");
     const [showMenu,
@@ -37,14 +24,10 @@ export function CategorySubCatDetailScree({route}) {
         image_link
     } = route.params
 
-   
-
     return (
         <VStack>
             <CategoryLeftAnimation _currentTab={currentTab} _setCurrentTab={setCurrentTab}/>
-            <CategoryRightAnimation _showMenu={showMenu} _setShowMenu={setShowMenu} _offsetvalue={offsetvalue}  _scalevalue={scalevalue} _closebuttonoffsetvalue={closebuttonoffsetvalue}/>
-
-           
+            <CategoryRightAnimation _showMenu={showMenu} _setShowMenu={setShowMenu} _offsetvalue={offsetvalue}  _scalevalue={scalevalue} _closebuttonoffsetvalue={closebuttonoffsetvalue}/>  
         </VStack>
 
     )
