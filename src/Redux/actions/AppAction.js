8ea8330c169c0cfc612b996931b2b08885944bee
Sanish Thapa <61194194+Sanish73@users.noreply.github.com>
2023-06-11 12:14:10 +0545

    l

--- src/Redux/actions/AppAction.js
+++
@@ -1,6 +0,0 @@
-export const loginStateChange = (state = false , data = {} , msg = "") => async dispatch  => {
-    dispatch({
-        type: 'LOGIN_STATE_CHANGED',
-        payload: { state  , data , msg}
-    });
-}
--- src/Redux/index.js
+++ src/Redux/index.js
@@ -1,14 +1,5 @@
-export * from './store';
-export * from './actions/loginAction';
-export * from './actions/AppAction';
+
 export * from './actions/brandAction';
-export * from './actions/attributeAction';
-export * from './actions/categoryAction';
-export * from './actions/productAction';
-export * from './actions/DisplayActions';
-export * from './actions/ordersAction';
-export * from './actions/attributeAction';
-export * from './actions/notificationAction';
 export * from './reducers';
 export const errorMsg = "Please check your internet connection and try again later.";
 
