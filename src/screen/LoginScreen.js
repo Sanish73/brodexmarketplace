939dd6cce81ad14eb21d36382cd7c2ecd7af1f12
Sanish Thapa <61194194+Sanish73@users.noreply.github.com>
2023-12-06 13:34:44 +0545

    asd

--- src/screen/LoginScreen.js
+++ src/screen/LoginScreen.js
@@ -9,12 +9,23 @@ const LoginScreen = () => {
     const page = 1;
     const refresh = true;
     const dispatch = useDispatch();
-    // const [email, setEmail] = useState(''); const [password, setPassword] =
-    // useState('');
-    const [isLoggedIn,
-        setIsLoggedIn] = useState(false);
-    const email = 'admin@admin.com';
-    const password = 'fancystore';
+    const [email,
+        setEmail] = useState('');
+    const [password,
+        setPassword] = useState('');
+    // const [isLoggedIn,     setIsLoggedIn] = useState(false); const email =
+    // 'admin@admin.com'; const password = 'fancystore';
+
+    const handleOnChangeText = (text) => {
+        setEmail(text);
+    }
+    const handleOnChangePass = (text) => {
+      setPassword(text);
+    }
+
+    console.log(email);
+    console.log(password);
+
     const handleLogin = () => {
         login('', email, password, page, refresh, function (dt) {
             if (dt
@@ -25,64 +36,72 @@ const LoginScreen = () => {
                 });
             } else 
                 (Alert.alert("Error", "Invalid Email or Password"))
-            })(dispatch);
-        };
-
-        return (
-            <View style={styles.container}>
-                <View>
-                
-                    <Text style={styles.heading}>Welcome</Text>
-                    <Text style={styles.subHeading}>Login to continue!</Text>
-                </View>
-                <View style={styles.form}>
-                    <Text style={styles.label}>Email</Text>
-                    <TextInput style={styles.input} value={email} placeholder="Enter your email"/>
-                    <Text style={styles.label}>Password</Text>
-                    <TextInput
-                        style={styles.input}
-                        value={password}
-                        placeholder="Enter your password"
-                        secureTextEntry/>
-                    <Button title="Login in" onPress={handleLogin} color="#4e67eb"/>
-                </View>
-            </View>
-        );
+        })(dispatch);
     };
+    return (
+        <View style={styles.container}>
+            <View>
+                <Text style={styles.heading}>Welcome</Text>
+                <Text style={styles.subHeading}>Login to continue!</Text>
+            </View>
+            <View style={styles.form}>
+                <Text style={styles.label}>Email</Text>
+                <TextInput
+                    style={styles.input}
+                    value={email}
+                    onChangeText={(text) => {
+                    handleOnChangeText(text)
+                }}
+                    placeholderTextColor="#3498db"placeholder="Enter your email"/>
+                <Text style={styles.label}>Password</Text>
+                <TextInput
+                    style={styles.input}
+                    value={password}
+                    placeholder="Enter your password"
+                    secureTextEntry
+                    onChangeText={(text) => {
+                    handleOnChangePass(text)
+                }}
+                    placeholderTextColor="#3498db"/>
+                <Button title="Login in" onPress={handleLogin} color="#4e67eb"/>
+            </View>
+        </View>
+    );
+};
 
-    const styles = StyleSheet.create({
-        container: {
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center'
-        },
-        heading: {
-            fontSize: 70,
-            fontWeight: 'bold',
-            marginBottom: 10,
-            color: 'black'
-        },
-        subHeading: {
-            fontSize: 16,
-            marginBottom: 20,
-            color: 'black'
-        },
-        form: {
-            width: '80%'
-        },
-        label: {
-            fontSize: 16,
-            marginBottom: 5,
-            color: 'black'
-        },
-        input: {
-            height: 40,
-            borderColor: '#ccc',
-            borderWidth: 1,
-            marginBottom: 20,
-            padding: 10,
-            color: 'black'
-        }
-    });
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    heading: {
+        fontSize: 70,
+        fontWeight: 'bold',
+        marginBottom: 10,
+        color: 'black'
+    },
+    subHeading: {
+        fontSize: 16,
+        marginBottom: 20,
+        color: 'black'
+    },
+    form: {
+        width: '80%'
+    },
+    label: {
+        fontSize: 16,
+        marginBottom: 5,
+        color: 'black'
+    },
+    input: {
+        height: 40,
+        borderColor: '#ccc',
+        borderWidth: 1,
+        marginBottom: 20,
+        padding: 10,
+        color: 'black'
+    }
+});
 
-    export default LoginScreen;
+export default LoginScreen;
